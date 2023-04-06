ce5ef59be3ca667015d0536e46c74e778d663baf
Syed Burhan Ali <syed.burhan8005@gmail.com>
2023-04-07 00:40:12 +0500

    Add Zero Before digits

--- app.js
+++ app.js
@@ -1,32 +1,34 @@
 function displayTime() {
-    var dateTime = new Date();
-    var hrs = dateTime.getHours();
-    var min = dateTime.getMinutes();
-    var sec = dateTime.getSeconds();
-    var session = document.getElementById('session')
-  
-    if (hrs >= 12) {
-      session.innerHTML = "PM";
-    } else {
-      session.innerHTML = "AM";
-    }
-  
-  
-    if (hrs > 12) {
-      hrs = hrs - 12;
-    }
-  
-    if (hrs == 0) {
-      hrs = 12;
-    }
-  
-  
-    document.getElementById('hours').innerHTML = hrs;
-    document.getElementById('minutes').innerHTML = min;
-    document.getElementById('seconds').innerHTML = sec;
+  var dateTime = new Date();
+  var hrs = dateTime.getHours();
+  var min = dateTime.getMinutes();
+  var sec = dateTime.getSeconds();
+  var session = document.getElementById('session')
+
+  if (hrs >= 12) {
+    session.innerHTML = "PM";
+  } else {
+    session.innerHTML = "AM";
   }
-  
-  setInterval(displayTime, 1000);
-  
-  
-  
+
+
+  if (hrs > 12) {
+    hrs = hrs - 12;
+  }
+
+  if (hrs == 0) {
+    hrs = 12;
+  }
+
+  hrs(hrs < 10) ? "0" + hrs : hrs;
+  min(min < 10) ? "0" + min : min;
+  hrs(sec < 10) ? "0" + sec : sec;
+
+  document.getElementById('hours').innerHTML = hrs;
+  document.getElementById('minutes').innerHTML = min;
+  document.getElementById('seconds').innerHTML = sec;
+}
+
+setInterval(displayTime, 1000);
+
+
