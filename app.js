8c993dfb7344a50fbb6163f3fac87c6943554bcf
Syed Burhan Ali <syed.burhan8005@gmail.com>
2023-04-07 23:25:45 +0500

    bux fixes

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-var dateTime = new Date();
-var hrs = dateTime.getHours();
-var min = dateTime.getMinutes();
-var sec = dateTime.getSeconds();
-var session = document.getElementById('session')
 
-if (hrs >= 12) {
-  session.innerHTML = "PM";
-} else {
-  session.innerHTML = "AM";
-}
 
 function displayTime() {
  
-
+  var dateTime = new Date();
+  var hrs = dateTime.getHours();
+  var min = dateTime.getMinutes();
+  var sec = dateTime.getSeconds();
+  var session = document.getElementById('session')
+  
+  if (hrs >= 12) {
+    session.innerHTML = "PM";
+  } else {
+    session.innerHTML = "AM";
+  }
 
   if (hrs > 12) {
     hrs = hrs - 12;
